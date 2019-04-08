ab2b86a1e63b5dda9e654569e6872d809dc3e5e5
ParkerSkarzynski <Skarzynp@mail.gvsu.edu>
2019-04-08 15:23:43 -0400

    Create Enemy and friendly troops

--- combat_AI.py
+++ combat_AI.py
@@ -9,18 +9,34 @@ class combat_stats:
 		print("in init")
 	
 
-	def getOdds(self,enemys,friendlies):
-		
-		for z in range(enemys):
-			for i in range(enemys):
-				Enemy=DnD.Creature("Wolf",base="owlbear",alignment='evil')
-			for j in range(friendlies):
-				Friendly=DnD.Creature("Dog",base="commoner",alignment='good')
+	def getOdds(self,enemies,friendlies):
+
+		friendList = []
+		enemyList = []
+		count = 0		
 
 		
-		rate=DnD.Encounter(Friendly,Enemy).predict()
-		print(rate)
+		for i in range(enemies):
+			Type = str(input("Type of enemies: "))				
+			enemyList.insert(i,Type)
+
+		for j in range(friendlies):
+			Type = str(input("Type of Friendlies: "))         
+			friendList.insert(j,Type)
+
+		for q in range(enemies):
+
+			Friendly= DnD.Creature(friendList[q])
+			Enemy = DnD.Creature(enemyList[q])
+			rate=DnD.Encounter(Friendly,Enemy).predict()
+#			print(rate[1])  
+#			print(rate[3])
+#			print(rate)
+			if(rate[1] > rate[3]):
+				count += 1
+				print("Count")
+				print(count)			
 		
-
-
+			
+			
 	
