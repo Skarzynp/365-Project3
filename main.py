69b4c170c8589758aba9730936cdee93f41d4e3c
Krueger <33267349+kruegeco@users.noreply.github.com>
2019-04-09 15:55:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,7 @@ import DnD
 print("AI Script")
 c = combat_stats()
 
-try:
-	enemies = int(input("How many enemies: "))
-except ValueError:
-	print("Not a Number")
-
-try:
-	friendlies = int(input("How many friendlies: "))
-except ValueError:
-	print("Not a Number")
-
-
-c.getOdds(enemies,friendlies)
+c.getOdds()
+c.getUserInput()
 
 
